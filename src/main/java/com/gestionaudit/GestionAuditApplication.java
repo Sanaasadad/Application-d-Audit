a6651dab0371264554096946e944e5b33565a296
Sanaasadad <sanaasadad4@gmail.com>
2025-05-24 20:23:59 +0100

    1ere version

---
+++ src/main/java/com/gestionaudit/GestionAuditApplication.java
@@ -0,0 +1,13 @@
+package com.gestionaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAuditApplication.class, args);
+    }
+
+}
